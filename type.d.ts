23afec3213634c4ce7e46851308dd48b93255872
gift56 <efeasiughu@gmail.com>
2023-09-26 11:33:17 -0400

    writing the schema for submitting form

--- type.d.ts
+++ type.d.ts
@@ -1,3 +1,12 @@
 export interface FormValue {
-  
+  coverImage: String | undefined;
+  firstName: String;
+  lastName: String;
+  emailId: String;
+  phoneNumber: Boolean;
+  nationality: Boolean;
+  currentResidence: Boolean;
+  idNumber: Boolean;
+  dateOfBirth: Boolean;
+  gender: Boolean;
 }
