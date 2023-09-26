cdefb4e8f5c17d7cb4684d5ae68344320bd3d161
gift56 <efeasiughu@gmail.com>
2023-09-26 13:22:11 -0400

    woking on card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import MainLayout from "./layout/MainLayout";
 import { tabLinks } from "./utils/constant";
+import { ApplicationForm } from "./components";
 
 const App = () => {
   const [tab, setTab] = useState("Application Form");
 
   return (
     <MainLayout>
-      <div className="w-full flex flex-col items-start justify-start h-screen  py-20 overflow-y-auto overflow-x-hidden">
+      <div className="w-full flex flex-col items-start justify-start h-screen py-20 overflow-y-auto overflow-x-hidden">
         <ul className="w-full h-[129px] shadow-tabShad flex items-center justify-start">
           {tabLinks.map((item) => (
             <li
@@ -23,7 +24,7 @@ const App = () => {
             </li>
           ))}
         </ul>
-        
+        <ApplicationForm />
       </div>
     </MainLayout>
   );
