06e6c5da9dd4682396b3a871a1aeb73adb9ce6ed
gift56 <efeasiughu@gmail.com>
2023-09-26 10:54:06 -0400

    added icons

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 const Sidebar = () => {
-  const sideBardIcon = ["/icon/homeIcon.png"];
+  const sideBardIcon = ["/icon/homeIcon.png", "/icon/taskIcon.png"];
   return (
     <ul className="w-full h-screen p-8 shadow-sideShad sticky top-0 flex flex-col items-center justify-start gap-8">
       <div className="cursor-pointer">
