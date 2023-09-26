4ea558f5ed2fe9020ba80bfe1223efb2491596c3
gift56 <efeasiughu@gmail.com>
2023-09-26 10:36:02 -0400

    adding some components

---
+++ type.d.ts
@@ -0,0 +1,9 @@
+export interface FormValue {
+    team_name: String;
+    phone_number: String;
+    email: String;
+    project_topic: String;
+    category: String;
+    group_size: String;
+    privacy_poclicy_accepted: boolean;
+  }
