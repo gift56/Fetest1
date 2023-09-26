8260f07fb89e50b2e55442247e109aa7ce5449a0
gift56 <efeasiughu@gmail.com>
2023-09-26 11:24:13 -0400

    working on registeration

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App = () => {
           {tabLinks.map((item) => (
             <li
               key={item}
-              className={`w-full h-full flex items-center justify-center relative transition-all duration-300 font-Inter text-lg font-medium cursor-pointer first:before:hidden before:content-[''] before:absolute before:w-[2px] before:h-[50%] before:bg-[#C4C4C4] before:right-0 ${
+              className={`w-full h-full flex items-center justify-center relative transition-all duration-300 font-Inter text-lg font-medium cursor-pointer first:before:hidden before:content-[''] before:absolute before:w-[2px] before:h-[50%] before:bg-[#C4C4C4] before:right-0 before:transition-all before:duration-300 ${
                 tab === item ? "bg-tabColor text-white before:bg-tabColor before:w-10 before:h-6 before:rotate-[45deg] before:translate-x-3" : ""
               } `}
             >
