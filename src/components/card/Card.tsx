e725d707392cecf9ed4d3fc784e76c5d6b5ffa5c
gift56 <efeasiughu@gmail.com>
2023-09-26 13:17:28 -0400

    writing the schema for submitting form

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,5 +1,12 @@
-const Card = () => {
-  return <div>Card</div>;
+import { ReactNode } from "react";
+
+interface CardProp {
+  headline: string;
+  content: ReactNode;
+}
+
+const Card = ({ headline, content }: CardProp) => {
+  return <div className="w-full lg:w-[595px] h-fit shadow-cardShad rounded-[20px]">Card</div>;
 };
 
 export default Card;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 import Sidebar from "./Sidebar";
+import Card from "./card/Card";
 import CustomizeInput from "./formInputs/CustomizeInput";
 import CustomizeTextarea from "./formInputs/CustomizeTextarea";
 
-export { Sidebar, CustomizeTextarea, CustomizeInput };
+export { Sidebar, CustomizeTextarea, CustomizeInput, Card };
