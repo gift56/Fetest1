bb7ac88736f6cfefcfb4cfb71cea8ae97be82ffb
gift56 <efeasiughu@gmail.com>
2023-09-26 15:39:38 -0400

    woking on card component

--- src/components/ApplicationForm/generalInitialValues.tsx
+++ src/components/ApplicationForm/generalInitialValues.tsx
@@ -1,7 +1,66 @@
 import { FormikValues } from "formik";
 
 const generalInitialValues = (): FormikValues => {
-  return <div>generalInitialValues</div>;
+  const initialValues: FormikValues = {
+    coverImage: "",
+    personalInformation: {
+      firstName: {
+        internalUse: false,
+        show: true,
+      },
+      lastName: {
+        internalUse: false,
+        show: true,
+      },
+      emailId: {
+        internalUse: false,
+        show: true,
+      },
+      phoneNumber: {
+        internalUse: false,
+        show: true,
+      },
+      nationality: {
+        internalUse: false,
+        show: true,
+      },
+      currentResidence: {
+        internalUse: false,
+        show: true,
+      },
+      idNumber: {
+        internalUse: false,
+        show: true,
+      },
+      dateOfBirth: {
+        internalUse: false,
+        show: true,
+      },
+      gender: {
+        internalUse: false,
+        show: true,
+      },
+      personalQuestions: [],
+    },
+    profile: {
+      education: {
+        mandatory: false,
+        show: true,
+      },
+      experience: {
+        mandatory: false,
+        show: true,
+      },
+      resume: {
+        mandatory: false,
+        show: true,
+      },
+      profileQuestions: [],
+    },
+    customisedQuestions: [],
+  };
+
+  return initialValues;
 };
 
 export default generalInitialValues;
