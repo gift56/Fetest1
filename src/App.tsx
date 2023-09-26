90e1adc6146723c6be6707333039cbf0dc9f3b20
gift56 <efeasiughu@gmail.com>
2023-09-26 11:12:40 -0400

    working on registeration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import MainLayout from "./layout/MainLayout";
+import { tabLinks } from "./utils/constant";
 
 const App = () => {
   const [tab, setTab] = useState("Application Form");
@@ -8,7 +9,9 @@ const App = () => {
     <MainLayout>
       <div className="w-full flex flex-col items-start justify-start h-screen  py-20 overflow-y-auto overflow-x-hidden">
         <div className="w-full h-[129px] shadow-tabShad flex items-center justify-start">
-          
+          {tabLinks.map((item)=>(
+            <div key={item}></div>
+          ))}
         </div>
       </div>
     </MainLayout>
