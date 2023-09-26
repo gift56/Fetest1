c7822ccfee0cad741747cbc16ccc06778576c78b
gift56 <efeasiughu@gmail.com>
2023-09-26 14:04:56 -0400

    woking on card component

--- src/components/ApplicationForm/ApplicationForm.tsx
+++ src/components/ApplicationForm/ApplicationForm.tsx
@@ -5,7 +5,13 @@ const ApplicationForm = () => {
   return (
     <form className="w-full flex flex-col p-5 md:p-10 gap-6">
       <Card headline="Upload cover image">
-        <div className="w-full border-dashed"></div>
+        <label className="w-full border border-black rounded-[5px] flex flex-col items-center justify-center px-3 border-dashed shadow-uploadShad h-[200px]">
+          <img
+            src=".icon/uploadIcon.png"
+            alt="uploadIcon"
+            className="w-8 h-8"
+          />
+        </label>
       </Card>
     </form>
   );
