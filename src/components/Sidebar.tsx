3c468b09ffcb680eaed5ba5b91b3535a33ce3f47
gift56 <efeasiughu@gmail.com>
2023-09-26 11:00:07 -0400

    added icons

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,19 +1,27 @@
 const Sidebar = () => {
   const sideBardIcon = ["/icon/homeIcon.png", "/icon/taskIcon.png"];
   return (
-    <ul className="w-full h-screen p-8 shadow-sideShad sticky top-0 flex flex-col items-center justify-start gap-16">
-      <div className="cursor-pointer">
-        <img src="/icon/barIcon.png" alt="barIcon" className="w-full" />
-      </div>
-      <nav
-        aria-label="navigate"
-        className="flex w-full flex-col items-center gap-6"
-      >
-        {sideBardIcon.map((item, i) => (
-          <img src={item} alt="barIcon" className="cursor-pointer w-full h-8" key={i} />
-        ))}
-      </nav>
-    </ul>
+    <div className="w-full h-screen p-8 shadow-sideShad sticky top-0 flex flex-col items-center justify-between">
+      <ul className="w-full flex flex-col items-center justify-center gap-16">
+        <div className="cursor-pointer">
+          <img src="/icon/barIcon.png" alt="barIcon" className="w-full" />
+        </div>
+        <nav
+          aria-label="navigate"
+          className="flex w-full flex-col items-center gap-6"
+        >
+          {sideBardIcon.map((item, i) => (
+            <img
+              src={item}
+              alt="barIcon"
+              className="cursor-pointer w-full h-8"
+              key={i}
+            />
+          ))}
+        </nav>
+      </ul>
+      <span></span>
+    </div>
   );
 };
 
