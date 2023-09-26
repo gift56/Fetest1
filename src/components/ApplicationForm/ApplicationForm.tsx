66035f73fb3569780d21f8ebc517eafa25bada4a
gift56 <efeasiughu@gmail.com>
2023-09-26 13:54:27 -0400

    woking on card component

--- src/components/ApplicationForm/ApplicationForm.tsx
+++ src/components/ApplicationForm/ApplicationForm.tsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
+import Card from "../card/Card";
 
 const ApplicationForm = () => {
-  return <form className="w-full flex flex-col p-5 md:p-10 gap-6">
-    hedn
-  </form>;
+  return (
+    <form className="w-full flex flex-col p-5 md:p-10 gap-6">
+      <Card headline="Upload cover image">
+        <div></div>
+      </Card>
+    </form>
+  );
 };
 
 export default ApplicationForm;
