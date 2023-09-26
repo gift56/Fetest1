7825875f95e71a8c3f6d56ebcdd69500faaa72cf
gift56 <efeasiughu@gmail.com>
2023-09-26 13:21:23 -0400

    woking on card component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,7 @@
+import ApplicationForm from "./ApplicationForm/ApplicationForm";
 import Sidebar from "./Sidebar";
 import Card from "./card/Card";
 import CustomizeInput from "./formInputs/CustomizeInput";
 import CustomizeTextarea from "./formInputs/CustomizeTextarea";
 
-export { Sidebar, CustomizeTextarea, CustomizeInput, Card };
+export { Sidebar, CustomizeTextarea, CustomizeInput, Card, ApplicationForm };
