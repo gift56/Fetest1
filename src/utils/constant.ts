39664c64d4e10ef0bb9316024fa4be80551c7d6e
gift56 <efeasiughu@gmail.com>
2023-09-26 15:59:15 -0400

    woking on card component

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,14 +1,13 @@
 export const questionType = [
   { text: "Paragraph", value: "Paragraph" },
   { text: "Short answer", value: "ShortAnswer" },
-  "",
-  "Yes/No",
-  "Dropdown",
-  "Multiple choice",
-  "Date",
-  "Number",
-  "File upload",
-  "Video question",
+  { text: "Yes/No", value: "YesNo" },
+  { text: "Dropdown", value: "Dropdown" },
+  { text: "Multiple choice", value: "MultipleChoice" },
+  { text: "Date", value: "Date" },
+  { text: "Number", value: "Number" },
+  { text: "File upload", value: "FileUpload" },
+  { text: "Video question", value: "FileUpload" },
 ];
 
 export const tabLinks = [
