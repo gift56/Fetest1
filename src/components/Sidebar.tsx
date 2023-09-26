eb9a2bf5d0d14572753c71387fd5b911f88992cd
gift56 <efeasiughu@gmail.com>
2023-09-26 10:51:26 -0400

    added icons

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,11 @@
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <ul className="w-full h-screen p-8 shadow-sideShad sticky top-0 flex flex-col items-center justify-start gap-8">
+      <div className="cursor-pointer">
+        <img src="/icon/barIcon.png" alt="barIcon" className="w-full" />
+      </div>
+    </ul>
+  );
 };
 
 export default Sidebar;
