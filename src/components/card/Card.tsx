6b100d732c8d85164001f8c1aa1142bba095cc5c
gift56 <efeasiughu@gmail.com>
2023-09-26 13:20:08 -0400

    woking on card component

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -11,6 +11,7 @@ const Card = ({ headline, content }: CardProp) => {
       <div className="bg-primary py-2 px-2">
         <h2 className="text-base md:text-lg font-semibold">{headline}</h2>
       </div>
+      <div className="w-full bg-white p-3">{content}</div>
     </div>
   );
 };
