4c912ae21edb8e7aed79361d14fff2d37eb9b740
gift56 <efeasiughu@gmail.com>
2023-09-26 11:14:42 -0400

    working on registeration

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,13 @@ const App = () => {
     <MainLayout>
       <div className="w-full flex flex-col items-start justify-start h-screen  py-20 overflow-y-auto overflow-x-hidden">
         <ul className="w-full h-[129px] shadow-tabShad flex items-center justify-start">
-          {tabLinks.map((item)=>(
-            <li key={item} className={`w-full h-full flex items-center justify-center`}></li>
+          {tabLinks.map((item) => (
+            <li
+              key={item}
+              className={`w-full h-full flex items-center justify-center relative transition-all duration-300`}
+            >
+              {item}
+            </li>
           ))}
         </ul>
       </div>
