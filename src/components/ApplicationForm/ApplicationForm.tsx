304fba29296d21853633699341959f00b0bcb008
gift56 <efeasiughu@gmail.com>
2023-09-26 13:45:02 -0400

    woking on card component

--- src/components/ApplicationForm/ApplicationForm.tsx
+++ src/components/ApplicationForm/ApplicationForm.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const ApplicationForm = () => {
-  return <form className="w-full flex flex-col p5 md:p-10">ApplicationForm</form>;
+  return <form className="w-full flex flex-col p-5 md:p-10">ApplicationForm</form>;
 };
 
 export default ApplicationForm;
