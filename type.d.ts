8e6dc9faaa39eff9ca017358b4a25f9026385f30
gift56 <efeasiughu@gmail.com>
2023-09-26 11:47:23 -0400

    writing the schema for submitting form

--- type.d.ts
+++ type.d.ts
@@ -1,12 +1,39 @@
-export interface FormValue {
-  coverImage: String | undefined;
-  firstName: String;
-  lastName: String;
-  emailId: String;
-  phoneNumber: Boolean;
-  nationality: Boolean;
-  currentResidence: Boolean;
-  idNumber: Boolean;
-  dateOfBirth: Boolean;
-  gender: Boolean;
+interface QuestionTemplate {
+  question: string;
+  choices?: string[]; 
 }
+
+interface ProfileTemplate {
+  mandatory: boolean;
+  show: boolean;
+}
+
+interface PersonalInformationTemplate {
+  internalUse: boolean;
+  show: boolean;
+}
+
+interface ApplicationForm {
+  coverImage: string;
+  personalInformation: {
+    firstName: PersonalInformationTemplate;
+    lastName: PersonalInformationTemplate;
+    emailId: PersonalInformationTemplate;
+    phoneNumber: PersonalInformationTemplate;
+    nationality: PersonalInformationTemplate;
+    currentResidence: PersonalInformationTemplate;
+    idNumber: PersonalInformationTemplate;
+    dateOfBirth: PersonalInformationTemplate;
+    gender: PersonalInformationTemplate;
+    personalQuestions: QuestionTemplate[];
+  };
+  profile: {
+    education: ProfileTemplate;
+    experience: ProfileTemplate;
+    resume: ProfileTemplate;
+    profileQuestions: QuestionTemplate[];
+  };
+  customisedQuestions: QuestionTemplate[];
+}
+
+export default ApplicationForm;
