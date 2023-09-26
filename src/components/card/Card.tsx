2700d6e64da10181ab93e2d820e818f2da24016d
gift56 <efeasiughu@gmail.com>
2023-09-26 13:19:44 -0400

    woking on card component

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -8,7 +8,9 @@ interface CardProp {
 const Card = ({ headline, content }: CardProp) => {
   return (
     <div className="w-full lg:w-[595px] h-fit shadow-cardShad rounded-[20px] flex flex-col items-start justify-start">
-      <h2 className="text-base md:text-lg font-semibold"></h2>
+      <div className="bg-primary py-2 px-2">
+        <h2 className="text-base md:text-lg font-semibold">{headline}</h2>
+      </div>
     </div>
   );
 };
