e286363bcbd180047f2e12f3a2985452656a2ce1
gift56 <efeasiughu@gmail.com>
2023-09-26 14:11:30 -0400

    woking on card component

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,12 @@ const App = () => {
   return (
     <MainLayout>
       <div className="w-full flex flex-col items-start justify-start h-screen py-20 overflow-y-auto overflow-x-hidden">
-        <ul className="w-full h-[129px] shadow-tabShad flex items-center justify-start">
+        <ul className="w-full shadow-tabShad flex items-center justify-start">
           {tabLinks.map((item) => (
             <li
               key={item}
               onClick={() => setTab(item)}
-              className={`w-full h-full flex items-center justify-center relative transition-all duration-300 font-Inter text-lg font-medium cursor-pointer first:before:hidden before:content-[''] before:absolute before:bg-[#C4C4C4] before:right-0 before:transition-all before:duration-300 ${
+              className={`w-full h-full flex items-center justify-center relative transition-all duration-300 font-Inter text-lg font-medium cursor-pointer first:before:hidden before:content-[''] before:absolute before:bg-[#C4C4C4] before:right-0 before:transition-all before:duration-300  py-9 ${
                 tab === item
                   ? "bg-tabColor text-white before:bg-tabColor before:w-10 before:h-6 before:rotate-[45deg] before:translate-x-3"
                   : "before:w-[2px] before:h-[50%]"
--- src/components/ApplicationForm/ApplicationForm.tsx
+++ src/components/ApplicationForm/ApplicationForm.tsx
@@ -5,7 +5,7 @@ const ApplicationForm = () => {
   return (
     <form className="w-full flex flex-col p-5 md:p-10 gap-6">
       <Card headline="Upload cover image">
-        <label className="w-full border border-black rounded-[5px] flex flex-col items-center justify-center px-3 border-dashed shadow-uploadShad h-[200px]">
+        <label className="w-full border border-black rounded-[5px] flex flex-col gap-2 cursor-pointer hover:bg-basegray/20 transition-all duration-300 items-center justify-center px-3 border-dashed shadow-uploadShad h-[200px] my-2">
           <img
             src="/icon/uploadIcon.png"
             alt="uploadIcon"
@@ -16,6 +16,9 @@ const ApplicationForm = () => {
           </h3>
           <p className="text-sm font-medium text-basegray">16:9 ratio is recommended. Max image size 1mb</p>
         </label>
+      </Card>
+      <Card headline="Personal Information">
+        
       </Card>
     </form>
   );
