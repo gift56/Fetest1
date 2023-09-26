90019e13063cec6788999c72b8a0ad44ab61e449
gift56 <efeasiughu@gmail.com>
2023-09-26 16:54:28 -0400

    working on validation

--- src/components/ApplicationForm/ApplicationForm.tsx
+++ src/components/ApplicationForm/ApplicationForm.tsx
@@ -453,7 +453,7 @@ const ApplicationForm = () => {
               <div className="flex items-center justify-start gap-2">
                 <input
                   type="checkbox"
-                  name="experience"
+                  name="profile.experience.mandatory"
                   id="experience"
                   className="accent-success rounded cursor-pointer w-4 h-4"
                 />
@@ -469,8 +469,8 @@ const ApplicationForm = () => {
                   <input
                     type="checkbox"
                     className="appearance-none"
-                    defaultChecked={false}
-                    // onChange={handleChange}
+                    defaultChecked={values.profile.experience.show}
+                    onChange={handleChange}
                     name="experience"
                   />
                   <i className="bg-[#F4F4F4] border relative w-11 h-6 rounded-xl transition-all duration-200 before:content-[''] before:absolute before:top-[1px] before:left-[2.8px] before:w-5 before:h-5 before:bg-white before:rounded-full before:shadow-newLongShadow before:transition-all before:duration-300 cursor-pointer"></i>
