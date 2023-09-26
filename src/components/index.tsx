5ebf91a0242443b473a9df5de95f0e5f98bec735
gift56 <efeasiughu@gmail.com>
2023-09-26 17:42:17 -0400

    working on the main logic now

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,6 +2,14 @@ import ApplicationForm from "./ApplicationForm/ApplicationForm";
 import Sidebar from "./Sidebar";
 import Card from "./card/Card";
 import CustomizeInput from "./formInputs/CustomizeInput";
+import CustomizeSelect from "./formInputs/CustomizeSelect";
 import CustomizeTextarea from "./formInputs/CustomizeTextarea";
 
-export { Sidebar, CustomizeTextarea, CustomizeInput, Card, ApplicationForm };
+export {
+  Sidebar,
+  CustomizeTextarea,
+  CustomizeInput,
+  Card,
+  ApplicationForm,
+  CustomizeSelect,
+};
