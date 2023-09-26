3c9b997447083b50fe16baff7c669e1cdebb1d12
gift56 <efeasiughu@gmail.com>
2023-09-26 17:56:34 -0400

    working on the main logic now

--- src/components/ApplicationForm/ApplicationForm.tsx
+++ src/components/ApplicationForm/ApplicationForm.tsx
@@ -453,7 +453,7 @@ const ApplicationForm = () => {
                 onChange={handleChange}
                 onBlur={handleBlur}
                 name={`item[${index}].type`}
-                className="bg-white appearance-none bg-[/public/icon/dropdownIcon.png] bg-no-repeat bg-[center_right_0.3rem] lg:bg-[center_right_1.2rem] border border-white h-[44px] w-full rounded px-4 outline-none text-sm shadow-registerShad text-white placeholder:text-borderColor focus:border-primary transition-all duration-300"
+                className="bg-white appearance-none bg-[/public/icon/dropdownIcon.png] bg-no-repeat bg-[center_right_0.3rem] lg:bg-[center_right_1.2rem] border border-white h-[44px] w-full rounded px-4 outline-none text-sm text-basegray placeholder:text-basegray focus:border-primary transition-all duration-300"
               >
                 {questionType.map((item) => (
                   <option id="type" value={item.value} key={item.text}>
@@ -461,6 +461,20 @@ const ApplicationForm = () => {
                   </option>
                 ))}
               </CustomizeSelect>
+              <CustomizeInput
+                showLabel={false}
+                label="Project Topic"
+                htmlFor="question"
+                labelClassName="text-sm font-normal"
+                type="text"
+                name={`item[${index}].question`}
+                value={item.question}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                id="question"
+                placeholder="Type here"
+                className="bg-white border border-white h-[44px] w-full rounded px-4 outline-none text-sm text-basegray placeholder:text-basegray focus:border-primary transition-all duration-300"
+              />
             </div>
           ))}
         </div>
