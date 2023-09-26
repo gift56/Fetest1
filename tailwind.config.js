47d8c050ee76b1bf9f45d4bd95a15d403d4cc707
gift56 <efeasiughu@gmail.com>
2023-09-26 09:48:59 -0400

    making changes on default templete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Inter: "'Inter', sans-serif",
+      },
+      colors: {
+        primary: "#D0F7FA",
+        basegray: "#979797",
+        danger: "#A80000",
+        success: "#087B2F",
+      },
+      boxShadow: {
+        cardShad: "3px 3px 14px 0px rgba(190, 190, 190, 0.30)",
+      },
+    },
   },
   plugins: [],
 };
