a87abf2b0e91ee084d26cad00f91e3e50f4b2f2a
gift56 <efeasiughu@gmail.com>
2023-09-26 20:26:53 -0400

    working on on the api

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,6 +1,8 @@
 import { baseUrl } from "../config";
 import { request } from "../lib/request";
 
-export const fetchDataFromServer = async () => {
+const fetchDataFromServer = async () => {
   return await baseUrl.get(request.requestapplication);
 };
+
+export default fetchDataFromServer;
