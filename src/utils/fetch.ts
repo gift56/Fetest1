4923ef92901ab2b5eed7c7855982f82d7ab60a4c
gift56 <efeasiughu@gmail.com>
2023-09-26 20:26:31 -0400

    working on on the api

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,1 +1,6 @@
-export {};
+import { baseUrl } from "../config";
+import { request } from "../lib/request";
+
+export const fetchDataFromServer = async () => {
+  return await baseUrl.get(request.requestapplication);
+};
