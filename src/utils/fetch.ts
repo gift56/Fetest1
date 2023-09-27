ec73692ca091b6b8c040b5602e44740deb4b41cc
gift56 <efeasiughu@gmail.com>
2023-09-26 21:05:07 -0400

    working on on the api

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,8 +1,13 @@
 import { baseUrl } from "../config";
+import FormValue from "../hooks/type";
 import { request } from "../lib/request";
 
 const fetchDataFromServer = async () => {
   return await baseUrl.get(request.requestapplication);
+};
+
+const updateServerData = async (payload: FormValue) => {
+  return await baseUrl.put(request.requestapplication, payload);
 };
 
 export { fetchDataFromServer };
